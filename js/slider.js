6c1af89816ebbc50076d65bd9e1339ea65c8d0a3
Vitaly39 <vgnatyk39@gmail.com>
2018-02-17 19:03:40 +0300

    выведем поведение слайдера в функцию

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,14 @@
 'use strict';
 
 (function () {
+  var MAX_SLIDER_VALUE = 455;
+  var MIN_SLIDER_VALUE = 1;
   var slider = document.querySelector('.upload-effect-level');
   var sliderPin = slider.querySelector('.upload-effect-level-pin'); // слайдер
   var sliderEffect = slider.querySelector('.upload-effect-level-val'); // полоса заполнения
 
-  sliderPin.addEventListener('mousedown', function (evt) {
+  // функция движения слайдера
+  var onEffectLevelPinMousedown = function (evt) {
     evt.preventDefault();
     var startCoordinate = evt.clientX;
     var onMouseMove = function (moveEvt) {
@@ -13,11 +16,11 @@
       var shiftX = startCoordinate - moveEvt.clientX;
       startCoordinate = moveEvt.clientX;
       sliderPin.style.left = (sliderPin.offsetLeft - shiftX) + 'px';
-      if (sliderPin.offsetLeft - shiftX >= 455) {
-        sliderPin.style.left = 455 + 'px';
+      if (sliderPin.offsetLeft - shiftX >= MAX_SLIDER_VALUE) {
+        sliderPin.style.left = MAX_SLIDER_VALUE + 'px';
       }
-      if (sliderPin.offsetLeft - shiftX <= 0) {
-        sliderPin.style.left = 1 + 'px';
+      if (sliderPin.offsetLeft - shiftX <= MIN_SLIDER_VALUE) {
+        sliderPin.style.left = MIN_SLIDER_VALUE + 'px';
       }
       sliderEffect.style.width = parseFloat(sliderPin.style.left) / 4.55 + '%';
     };
@@ -28,6 +31,9 @@
     };
     slider.addEventListener('mousemove', onMouseMove);
     slider.addEventListener('mouseup', onMouseUp);
-  });
+  };
+
+  // обработчик событий для пина слайдера
+  sliderPin.addEventListener('mousedown', onEffectLevelPinMousedown);
 
 })();
