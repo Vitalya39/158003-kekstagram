ac6a5679067dfd8a8bc1f27197aff929002fae72
Vitaly39 <vgnatyk39@gmail.com>
2018-02-22 16:08:52 +0300

    сделай возможность загрузить форму на сервер

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@
   var fileInput = document.querySelector('#upload-file');
   var editImageOverlay = document.querySelector('.upload-overlay');
   var closeImageOverlay = editImageOverlay.querySelector('#upload-cancel');
+  var form = document.querySelector('.upload-form');
 
   var openForm = function () {
     editImageOverlay.classList.remove('hidden');
@@ -28,5 +29,32 @@
     closeForm();
     document.removeEventListener('keydown', closeFormOnEsc);
   });
+
+  form.addEventListener('submit', onFormClick);
+
+  var onSuccessSend = function () {
+    closeForm();
+  };
+
+  var onErrorSend = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var onFormClick = function (evt) {
+    evt.preventDefault();
+    if (window.validity.isValidHashtags()) {
+      window.backend.upload(new FormData(form), onSuccessSend, onErrorSend);
+    }
+  };
+
+  form.addEventListener('submit', onFormClick);
 
 })();
